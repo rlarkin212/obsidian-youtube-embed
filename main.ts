e20982718cf87047554142ec052ca05ad5144af1
Ryan Larkin <rlarkin212@gmail.com>
2022-06-24 19:43:03 +0100

    plugin working

--- main.ts
+++ main.ts
@@ -1,85 +1,38 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	mode: embedMode
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+enum embedMode {
+	clipboard = "Clipboard",
+	highlighted = "Highlighted",
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: PluginSettings = {
+	mode: embedMode.clipboard
+}
+
+export default class YoutubeEmbed extends Plugin {
+	settings: PluginSettings
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new SettingsTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: "embed-youtube-video",
+			name: "Embed Youtube Video",
+			hotkeys: [
+				{
+					modifiers: ["Meta", "Alt"],
+					key: "E"
 				}
-			}
+			],
+			editorCallback: (editor: Editor) => {
+				this.embed(editor)
+			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,49 +42,67 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async embed(editor: Editor) {
+		if (this.settings.mode == embedMode.clipboard) {
+			const input = await navigator.clipboard.readText();
+			const iFrame = await this.formatIFrame(input)
+
+			if (iFrame != "") {
+				editor.replaceRange(iFrame, editor.getCursor());
+			} else {
+				new Notice("Link was not a valid youtube watch link")
+			}
+		}
+
+		if (this.settings.mode == embedMode.highlighted) {
+			const input = editor.getSelection()
+			const iFrame = await this.formatIFrame(input)
+
+			if (iFrame != "") {
+				editor.replaceSelection(iFrame, editor.getSelection())
+			} else {
+				new Notice("Link was not a valid youtube watch link")
+			}
+		}
+	}
+
+	async formatIFrame(input: string): Promise<string> {
+		let iFrame = "";
+
+		if (input.contains("https://www.youtube.com/")) {
+			const url = input.split('?v=')[1]
+
+			iFrame = `<iframe src="https://www.youtube.com/embed/${url}?feature=oembed" height="113" width="200" style="aspect-ratio: 1.76991 / 1; width: 100%; height: 100%;"></iframe>`
+		}
+
+		return iFrame;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingsTab extends PluginSettingTab {
+	plugin: YoutubeEmbed;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: YoutubeEmbed) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const {containerEl} = this;
 
+	display(): void {
+		const { containerEl } = this;
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
+		containerEl.createEl('h2', { text: 'Youtube Embed Settings' });
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+			.setName('Embed Mode')
+			.addDropdown(dropdown => dropdown
+				.addOption(embedMode.clipboard, embedMode.clipboard)
+				.addOption(embedMode.highlighted, embedMode.highlighted)
+				.setValue(this.plugin.settings.mode)
+				.onChange(async (value: string) => {
+					this.plugin.settings.mode = value as embedMode
+					await this.plugin.saveSettings()
 				}));
 	}
 }
